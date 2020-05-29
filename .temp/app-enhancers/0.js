09f2b81ea842e59fea17813e7b2da638a45d6425
changhaojun <648859757@qq.com>
2020-05-29 18:02:56 +0800

    删除文件 .temp

--- .temp/app-enhancers/0.js
+++
@@ -1,2 +0,0 @@
-import "C:\\Users\\64885\\AppData\\Roaming\\npm\\node_modules\\vuepress\\node_modules\\_@vuepress_core@1.5.0@@vuepress\\core\\lib\\node\\internal-plugins\\style\\client.js"
-export default {}
--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("C:\\Users\\64885\\AppData\\Roaming\\npm\\node_modules\\vuepress\\node_modules\\_@vuepress_theme-default@1.5.0@@vuepress\\theme-default\\global-components\\Badge"))
-
-
-export default {}
